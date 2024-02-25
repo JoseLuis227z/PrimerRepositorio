6d4621b7513b7566785dba9c6c0c87f286bbdf51
JoseLuis227z <Jugalde227z@fpdrioja.com>
2024-02-25 11:16:36 +0100

    Version final

--- TareaUD04/src/tareaUD4/Main.java
+++ TareaUD04/src/tareaUD4/Main.java
@@ -2,20 +2,39 @@ package tareaUD4;
 
 import java.util.Scanner;
 
+/**
+* Clase Main
+* Es la clase principal
+* @author José Luis Ugalde / Entorno de desarrollo
+* @version 0.1, 2024/02/25
+*/
+
 public class Main {
+	
+	/**Método de la clase principal
+	 * Descripcion: Crea un objeto de la clase Persona
+	 * Pide por teclado que se introduzca una edad y se almacena en el atributo edad
+	 * del objeto persona1 perteneciente a la clase Persona
+	 *  @param args proporciona argumentos de línea de comandos como una matriz de objetos String */
+	
 
 	public static void main(String[] args) {
+		
 		
 		Persona persona1=new Persona();
 		
 		Scanner leer= new Scanner(System.in);
 		
+		do {
+		
 		System.out.println("Introduce tu edad");
 		
 		persona1.edad=leer.nextInt();
+		
+		}while((persona1.edad<16)&&(persona1.edad>67));
 	
 		
-		persona1.calcularAñosHastaJubilacion(persona1);
+		persona1.calcularAniosHastaJubilacion(persona1);
 		
 		
 
--- TareaUD04/src/tareaUD4/Persona.java
+++ TareaUD04/src/tareaUD4/Persona.java
@@ -1,12 +1,40 @@
 package tareaUD4;
 
+/**Clase Persona, consta de un atributo edad de tipo entero y un método 
+ * llamado calcularAniosHastaJubilacion
+ * @author Jose Luis Ugalde
+ * @version 0.1 25/02/2024*/
+  
 public class Persona {
 	
-	int edad;
+	/**Atributo edad de tipo entero
+	 * Valores válidos: Mayor que 16 y menor de 67
+	 * Se inicializa en valor 16*/
+	int edad=16;
 	
-	public void calcularAñosHastaJubilacion(Persona persona1) {
+	
+	/**
+	* Método que calcula los años que faltan hasta la jubilación
+	* Una vez introducida la edad por teclado se le pasa mediante el parametro
+	* de tipo Persona al metodo y calculará los años restantes hasta la jubilación.
+	* @param  persona1 objeto de tipo Persona
+	* @author José Luis Ugalde
+	*/
+	
+	
+
+	public void calcularAniosHastaJubilacion(Persona persona1) {
 		
+		/**Variable texto de tipo String
+		 * Muestra por pantalla uno de los dos comentarios
+		 * Solo te quedan 7 años. ¡ Animo que ya queda poco !
+		 * Te quedan 23 años. ¡ A seguir trabajando gandúl !
+		 * Se inicializa a un texto vacío */
 		String texto="";
+		
+		/**Variable aniosRestantes de tipo entero
+		 * Almacena la diferencia entre la edad de jubilación y la edad introducida por el ususario
+		 * */
 		int aniosRestantes=67-persona1.edad;
 		
 		if (aniosRestantes<10) {
